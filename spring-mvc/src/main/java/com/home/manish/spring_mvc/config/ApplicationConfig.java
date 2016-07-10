27d6ba9b18c13f8a9803213cef1986fb23ceff9b
manishjain888 <manishjain888@gmail.com>
2016-07-10 07:08:52 +0100

    Spring mvc by archtype

---
+++ spring-mvc/src/main/java/com/home/manish/spring_mvc/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.home.manish.spring_mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import com.home.manish.spring_mvc.Application;
+
+@Configuration
+@PropertySource("classpath:persistence.properties")
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackageClasses = Application.class)
+class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
